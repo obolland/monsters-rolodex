9c71b098872056d92a9c15b3ff93e5ec700ca83e
Olly Bolland <Olly@Ollys-MacBook-Pro.local>
2020-11-17 14:41:00 +0300

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
+import { SearchBox } from './components/search-box/search-box.component';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => this.setState({ monsters: users }))
+}
+
+//writing methods as arrow functions means .this is automatically bound in the context in which it was created
+//as this class, 'App' is the current context, this is automatically bound to to 'App' class
+//...there is no need to bind .this within the constructor
+handleChange = (e) => {
+  this.setState({ searchField: e.target.value })
+}
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monsters Rolodex</h1>
+      <SearchBox 
+      placeholder = "search monsters..."
+      handleChange = { this.handleChange }
+      />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
+}
 }
 
 export default App;
